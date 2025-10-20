caba7343f2f9b9f5902cf7896373f0477ce0f482
Sabadash Viktoriia <sabadashvika2006@gmail.com>
2025-10-21 00:22:22 +0300

    add output messages

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,24 @@
 #include "calculator.h"
+#include <iostream>
 
 int Calculator::Add (double a, double b)
 {
-	return a + b + 0.5;
+    int result = a + b + 0.5;
+    std::cout << "Add: " << a << " + " << b << " = " << result << std::endl;
+    return result;
 }
 
 int Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+    int result = Add(a, -b);
+    std::cout << "Sub: " << a << " - " << b << " = " << result << std::endl;
+    return result;
 }
 
 int Calculator::Mul (double a, double b)
 {
-    return a * b + 0.5;
+    int result = a * b + 0.5;
+    std::cout << "Mul: " << a << " * " << b << " = " << result << std::endl;
+    return result;
 }
+
